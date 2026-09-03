70d563f3cf37b1985155835762201cf06e1fdee8
anggunnkaguraa <anggunnpermatasaari1012@gmail.com>
2026-09-04 06:07:00 +0700

    pertemuan2

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+use App\Http\Controllers\DashboardController;
+
+ 
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware(['auth'])
+    ->name('dashboard');
+
+Route::get('/', function (){
+    return view('welcome');
+}
+);
+
+
+Route::get('/about', function() {
+    return 'Halo, ini profil POS Barokah Mart.';
+});
